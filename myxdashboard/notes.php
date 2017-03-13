3ebf3c5496a02c102dc92baec164a6992730e8ba
Ted Yuen <tedyuen.goo@gmail.com>
2017-03-13 11:05:49 +0800

    Add myxdashboard template.

---
+++ myxdashboard/notes.php
@@ -0,0 +1,35 @@
+MyxDashboard is a clean and elegant template suitable for any back-end application. 
+Created using latest Bootstrap Framework.
+
+It is intended to be lightweight and easy to use and customize.
+
+DEMO: http://myxvip.com
+
+Main Features:
+Built on Bootstrap 3.3.1
+Font Awesome Ready
+Live Graph
+Morris chart
+jVector Map
+Data Tables
+Invoice Page
+Sign In and Login Pages
+
+Regular Updates:
+No need to worry about subscription or renewal fees. Once you buy a theme, you receive upgrades at no cost.
+
+Changelog: 
+Version 1.0 
+- Initial release
+
+Version 2.0 
+- Bootstrap 3.3.1
+- Added Morris chart
+
+
+Credits:
+Kirill Lebedev - jVectorMap
+Ben Alman - Flot Chart
+Imcreator.com - Free Images
+Getbootstrap.com
+Font Awesome
